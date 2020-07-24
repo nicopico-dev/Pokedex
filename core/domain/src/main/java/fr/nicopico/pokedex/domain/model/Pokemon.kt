4f429bcd1c3ae60c2b87c61bc01369207f238370
Nicolas Picon <nicopico.dev@gmail.com>
2020-07-24 21:30:22 +0200

    Add typealias for PokemonId

--- core/domain/src/main/java/fr/nicopico/pokedex/domain/model/Pokemon.kt
+++ core/domain/src/main/java/fr/nicopico/pokedex/domain/model/Pokemon.kt
@@ -1,7 +1,7 @@
 package fr.nicopico.pokedex.domain.model
 
 data class Pokemon(
-    val id: Int,
+    val id: PokemonId,
     val name: String
 ) {
     val imageUrl: String
--- core/domain/src/main/java/fr/nicopico/pokedex/domain/model/aliases.kt
+++ core/domain/src/main/java/fr/nicopico/pokedex/domain/model/aliases.kt
@@ -1,4 +1,6 @@
 package fr.nicopico.pokedex.domain.model
 
+typealias PokemonId = Int
 typealias Weight = Double
 typealias Height = Double
+typealias HexColor = String
