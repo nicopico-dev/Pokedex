0d510cda592ab451334b5f23fc8e7f68139e10de
Nicolas Picon <nicopico.dev@gmail.com>
2020-08-12 23:04:18 +0200

    Add infra for ViewModel injection

---
+++ base/android-di/src/main/java/fr/nicopico/base/android/dependency/injection/ViewModelFactoryProducerHolder.kt
@@ -0,0 +1,18 @@
+package fr.nicopico.base.android.dependency.injection
+
+import androidx.lifecycle.ViewModelProvider
+
+private typealias FactoryProducer = () -> ViewModelProvider.Factory
+
+object ViewModelFactoryProducerHolder {
+
+    val injectedFactoryProducer: FactoryProducer
+        get() = _factoryProducer
+            ?: throw IllegalStateException("FactoryProducer must be initialized by calling the init() method")
+
+    private var _factoryProducer: FactoryProducer? = null
+
+    fun init(factoryProducer: FactoryProducer) {
+        this._factoryProducer = factoryProducer
+    }
+}
--- feature/pokemon-list/src/main/java/fr/nicopico/pokedex/feature/pokemon/list/ui/PokemonListFragment.kt
+++ feature/pokemon-list/src/main/java/fr/nicopico/pokedex/feature/pokemon/list/ui/PokemonListFragment.kt
@@ -5,16 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import fr.nicopico.base.android.dependency.injection.injectedViewModels
 import fr.nicopico.pokedex.feature.pokemon.list.databinding.PokemonListFragmentBinding
 
 class PokemonListFragment : Fragment() {
 
     private lateinit var binding: PokemonListFragmentBinding
 
-    private val viewModel: PokemonListViewModel by viewModels()
+    private val viewModel: PokemonListViewModel by injectedViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
