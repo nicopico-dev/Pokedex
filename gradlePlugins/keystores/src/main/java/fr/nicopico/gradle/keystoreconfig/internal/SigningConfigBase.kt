298c0c0a7b4315523245e44642cdc6ad88ff4e07
Nicolas Picon <nicopico.dev@gmail.com>
2020-10-25 15:21:13 +0100

    Build: Keystores - app build :D / tests broken :(

--- gradlePlugins/keystores/src/main/java/fr/nicopico/gradle/keystoreconfig/internal/SigningConfigBase.kt
+++ gradlePlugins/keystores/src/main/java/fr/nicopico/gradle/keystoreconfig/internal/SigningConfigBase.kt
@@ -2,11 +2,17 @@ package fr.nicopico.gradle.keystoreconfig.internal
 
 import java.security.KeyStore
 import com.android.build.api.dsl.SigningConfig as SigningConfigDsl
+import com.android.build.gradle.internal.dsl.SigningConfig as SigningConfigInternal
 
+/*
+ * Gradle Android Plugin 4.1.0 expect [SigningConfigInternal] (implementation)
+ * instead of [SigningConfigDsl]
+ * TODO Only extends [SigningConfigDsl] once possible
+ */
 @Suppress("UnstableApiUsage")
 internal abstract class SigningConfigBase(
     private val name: String
-) : SigningConfigDsl {
+) : SigningConfigInternal(name), SigningConfigDsl {
 
     override fun getName(): String = name
     override var storeType: String? = KeyStore.getDefaultType()
